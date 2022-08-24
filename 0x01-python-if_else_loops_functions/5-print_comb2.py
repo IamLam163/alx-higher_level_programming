5d4dc93bf5a4cabfb375d022b2acd68cb5d3f785
IamLam163 <olamidealiyu@gmail.com>
2022-08-23 19:30:53 -0700

    challenge

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num <= 9:
-        print("0{}".format(num))
+    if num != 99:
+        print("{:02d},".format(num), end='')
     else:
-        print("{}".format(num))
+        print("{:02d},".format(num))
