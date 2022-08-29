c5090c647a7b798e64f42d711af9aa675ef338a8
IamLam163 <104768490+IamLam163@users.noreply.github.com>
2022-08-29 22:27:06 +0100

    No C or c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 def no_c(my_string):
     new_string = list(my_string)
     index_count = 0
-
     for index in new_string:
         if index == 'c' or index == 'C':
             new_string[index_count] = ""
-            index_count += 1
-        return "".join(new_string)
+        index_count += 1
+    return "".join(new_string)
