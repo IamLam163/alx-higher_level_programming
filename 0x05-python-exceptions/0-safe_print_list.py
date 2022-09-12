8d0bec50bb4e4ceb8ea060b9233142d3487a4e78
IamLam163 <olamidealiyu@gmail.com>
2022-09-12 10:43:34 -0700

    print len of list using iterator

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
-        try:
-            print("{}".format(my_list[idx]), end="")
-        except IndexError:
-            break
-        idx += 1
-    print(" ")
-    return idx
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    try:
+        num = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            num += 1
+            for x in range(num):
+                print("", end="")
+        print()
+        return num
+    except IndexError:
+        print()
+        return num
