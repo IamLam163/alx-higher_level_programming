293cdd4c91270f2ec0576caaacc61b2cfc5d35b8
IamLam163 <olamidealiyu@gmail.com>
2022-09-12 10:32:58 -0700

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
     while idx < x
     try:
-        print("{}",format.my_list[idx])
+        print("{}", format.my_list[idx])
     except ValueError:
         break
     idx += 1
