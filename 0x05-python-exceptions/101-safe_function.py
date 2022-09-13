1b1d2690ef39472440a28861ce9dbe24bd03e79c
IamLam163 <olamidealiyu@gmail.com>
2022-09-12 19:42:50 -0700

    execute function safely using command line args

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def safe_function(fct,*args):
+def safe_function(fct, *args):
     import sys
     try:
         result = fct(*args)
     except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
-        return None
-    return (result)
+        result = None
+
+    return (result)`
