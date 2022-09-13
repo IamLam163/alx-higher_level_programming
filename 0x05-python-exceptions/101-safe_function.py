3a8fb16ae0a6d9d0a29b0ab91b1b16d12ba2287d
IamLam163 <olamidealiyu@gmail.com>
2022-09-12 19:41:24 -0700

    execute function safely using command line args

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct,*args):
     import sys
     try:
         result = fct(*args)
-    except (ZeroDivisionError, IndexError, TypeError, ValueError) as i:
+    except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
-        return = None
+        return None
     return (result)
