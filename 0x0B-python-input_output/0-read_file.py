1cc80f61d4488cf554adaa71b028793906cb4318
IamLam163 <olamidealiyu@gmail.com>
2022-09-30 10:47:12 -0700

    function that reads and prints contents from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-function that reads and prints content from a file
+Contains function that reads and prints contents from file
 """
 
 
 def read_file(filename=""):
+    """Read and print text from file"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
