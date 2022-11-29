a343a0a05162d19753c80840de4c24cc70295c8e
IamLam163 <olamidealiyu@gmail.com>
2022-11-29 16:01:08 +0100

    sorting in JS

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const arg = parseInt(process.argv.length);
+
+function findbig(a, b) {
+  return (b - a);
+}
+
+if (isNaN(arg) || arg <= 3) {
+  console.log(0);
+} else {
+  console.log(process.argv.slice(2).sort(findbig)[1]);
+} 
