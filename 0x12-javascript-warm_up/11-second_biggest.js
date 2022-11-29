5be763fb9d9fec776f59a9b65869a8ff9da0510a
IamLam163 <olamidealiyu@gmail.com>
2022-11-29 16:02:26 +0100

    sorting in JS

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const arg = parseInt(process.argv.length);
 
-function findbig(a, b) {
+function findbig (a, b) {
   return (b - a);
 }
 
@@ -10,4 +10,4 @@ if (isNaN(arg) || arg <= 3) {
   console.log(0);
 } else {
   console.log(process.argv.slice(2).sort(findbig)[1]);
-} 
+}
