c09786e9b96afefac0123e27e428ecbcb2889502
IamLam163 <olamidealiyu@gmail.com>
2022-11-30 17:05:42 +0100

    s from Square of 5-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,8 @@
 #!/usr/bin/node
+const square = require('./5-square.js');
 
-class Rectangle {
-  constructor(w, h) {
-    if (w && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate() {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  double() {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-    // could also be written as this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor(size) {
-    super(size, size);
-  }
-
-  charPrint(c) {
+class Square extends square {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
@@ -41,5 +11,5 @@ class Square extends Rectangle {
       }
     }
   }
-};
+}
 module.exports = Square;
