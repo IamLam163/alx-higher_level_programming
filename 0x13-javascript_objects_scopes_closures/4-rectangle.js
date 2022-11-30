29e92bc9f92db46419bb1ba02444f5cddfc58d09
IamLam163 <olamidealiyu@gmail.com>
2022-11-30 14:23:24 +0100

    double and rotate methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+    //could also be written as this.width *= 2;
+  }
+}
+module.exports = Rectangle;
