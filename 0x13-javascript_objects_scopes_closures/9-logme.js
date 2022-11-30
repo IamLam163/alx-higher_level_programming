4243c947b288a5df44958460ad83fc0e81fa2ba2
IamLam163 <olamidealiyu@gmail.com>
2022-11-30 22:49:08 +0100

    print arguments

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 let args = 0;
-exports.logMe = function(item) {
-  console.log(args + ": " + item);
+exports.logMe = function (item) {
+  console.log(args + ': ' + item);
   args++;
 };
-
