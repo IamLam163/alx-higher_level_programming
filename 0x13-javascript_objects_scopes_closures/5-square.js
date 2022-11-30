318855db6dc498449e7f596c4428ba4d611e1d8f
IamLam163 <olamidealiyu@gmail.com>
2022-11-30 16:16:39 +0100

    inheriting from another class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,37 +1,36 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
 
-  double() {
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
     // could also be written as this.width *= 2;
   }
 }
 
-
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
-    //this.size = size;
+    // this.size = size;
   }
 }
 module.exports = Square;
