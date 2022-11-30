333d5be9da0813e3d82f8e61b19e234b82277878
IamLam163 <olamidealiyu@gmail.com>
2022-11-30 23:13:24 +0100

    concatenate two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
 
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 const fs = require('fs');
-
-fs.readFile(myArgs[0], 'utf-8', function(err, data) {
-  if (err) {
-    throw err;
-  }
-  fs.readFile(myArgs[1], 'utf-8', function(err2, data2) {
-    if (err2) {
-      throw err2;
-    }
-    const str = data + data2;
-    fs.writeFile(myArgs[2], str, function() { });
-  });
-});
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
