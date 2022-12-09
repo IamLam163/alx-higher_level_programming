f2a2d151972043cb61d450c8359df4c0eee6eac8
IamLam163 <olamidealiyu@gmail.com>
2022-12-09 21:02:21 +0100

    display states by arguments

--- 0x0F-python-object_relational_mapping/2-filter_states.py
+++ 0x0F-python-object_relational_mapping/2-filter_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+
+"""
 import sys
-from sys import argv
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",port=3306,db="hbtn_0e_0_usa",read_default_file="~/.my.cnf")
-#db = MySQLdb.connect(user=sys.argv[1],password=sys.argv[2],database=sys.argv[3], port=3306)
-
-#Use database, select all states
-cur = db.cursor()
-
-
 if __name__ == "__main__":
-
-    cur.execute("SELECT * FROM `states` WHERE Name LIKE BINARY '{:s}' ORDER BY id ASC".format(argv[4]))
-
-
-    for state in cur.fetchall():
-            if state[1] == argv[4]:
-                print(state)
-
-cur.close()
-db.close()
-#print(rows)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], port=3306, db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE'{:s}'\
+            ORDER BY id ASC".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
