0a8110a58892845a9639761b1c1d9deaa0f0d97e
IamLam163 <olamidealiyu@gmail.com>
2022-12-09 21:18:07 +0100

    ignore

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
