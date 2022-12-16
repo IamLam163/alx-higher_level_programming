8c2e8e30e9744ef7f5aaed232fe8fffeb7987cdf
IamLam163 <olamidealiyu@gmail.com>
2022-12-16 06:01:55 +0100

    push

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and sends a GET request
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" 
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1" 
