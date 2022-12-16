7e806f3c3680d10dbddc25b9d381f7cb5ee07022
IamLam163 <olamidealiyu@gmail.com>
2022-12-16 17:40:50 +0100

    payload

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script takes a URL and sends a POST request"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
