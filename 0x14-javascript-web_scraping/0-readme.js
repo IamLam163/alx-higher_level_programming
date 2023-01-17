9518fa26df6084985e678ef7f73db980cbf8629b
IamLam163 <olamidealiyu@gmail.com>
2023-01-17 14:17:41 +0100

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 // script reads and prints the contents of a file
 
-
-const fs = require('fs')
+const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function(err, data) {
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
