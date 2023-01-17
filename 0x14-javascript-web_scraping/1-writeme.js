dcc087b752529f718a0747758ac6ab8b7c87caf1
IamLam163 <olamidealiyu@gmail.com>
2023-01-17 14:25:12 +0100

    write to a file in JS

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script writes a string to a file
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filepath, string, 'utf-8', function(err, input) {
+  if (err) console.error(err);
+});
