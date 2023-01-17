1e5b27d60744fc0810db12f1c77c16eb33c1c99b
IamLam163 <olamidealiyu@gmail.com>
2023-01-17 14:26:21 +0100

    linter

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const filepath = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(filepath, string, 'utf-8', function(err, input) {
+fs.writeFile(filepath, string, 'utf-8', function (err, input) {
   if (err) console.error(err);
 });
